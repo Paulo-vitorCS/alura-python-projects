7dc84ecc2e7a2f22269b6d9b3b7b63a29518369e
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2022-10-28 14:52:21 -0300

    Starting implementation of forca

--- 01-Jogos/forca.py
+++ 01-Jogos/forca.py
@@ -1,9 +1,29 @@
 def jogar():
     print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
+    print("*  Bem vindo ao jogo da Forca!  *")
     print("*********************************")
+
+    palavra_secreta = "banana"
+    letras_acertadas = ['_'] * len(palavra_secreta)
+
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        print(letras_acertadas)
+
+        chute = input("\nQual letra: ").strip()
+
+        posicao = 0
+        for letra in palavra_secreta:
+            if chute.lower() == letra.lower():
+                letras_acertadas[posicao] = chute
+            posicao += 1
 
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
+
+# Caso queira rodar diretamente forca.py
+if __name__ == "__main__":
     jogar()
+
