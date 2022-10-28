f93155ed63433045bc4ce2c18e74504eb840ef85
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2022-10-28 17:05:26 -0300

    Improving the hangman game

--- 01-Jogos/forca.py
+++ 01-Jogos/forca.py
@@ -3,27 +3,39 @@ def jogar():
     print("*  Bem vindo ao jogo da Forca!  *")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".lower()
     letras_acertadas = ['_'] * len(palavra_secreta)
+    erros = 0
 
     enforcou = False
     acertou = False
 
+    print(letras_acertadas)
+
     while not enforcou and not acertou:
+        chute = input("\nQual letra? ").strip().lower()
+
+        if chute in palavra_secreta:
+            posicao = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[posicao] = chute
+                posicao += 1
+        else:
+            erros += 1
+
         print(letras_acertadas)
 
-        chute = input("\nQual letra: ").strip()
+        enforcou = erros == len(palavra_secreta)
+        acertou = '_' not in letras_acertadas
 
-        posicao = 0
-        for letra in palavra_secreta:
-            if chute.lower() == letra.lower():
-                letras_acertadas[posicao] = chute
-            posicao += 1
-
-    print("Fim do jogo")
+    if acertou:
+        print('\n*** Você ganhou! ***')
+    else:
+        print('\n*** Você perdeu! ***')
+    print("\nFim do jogo")
 
 
 # Caso queira rodar diretamente forca.py
 if __name__ == "__main__":
     jogar()
-
