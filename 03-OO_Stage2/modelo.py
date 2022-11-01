1f2171afd8fb7ad6b322687eae2bab1a18777211
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2022-11-01 08:26:22 -0300

    Overriding methods

--- 03-OO_Stage2/modelo.py
+++ 03-OO_Stage2/modelo.py
@@ -19,11 +19,17 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def imprime(self):
+        print(f'Nome: {self.nome} | Ano: {self.ano} | {self.likes} Likes')
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def imprime(self):
+        print(f'Nome: {self.nome} | Ano: {self.ano} | Duração: {self.duracao} | {self.likes} Likes')
 
 
 class Serie(Programa):
@@ -31,9 +37,13 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def imprime(self):
+        print(f'Nome: {self.nome} | Ano: {self.ano} | Temporadas: {self.temporadas} | {self.likes} Likes')
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
+
 vingadores.dar_likes()
 vingadores.dar_likes()
 vingadores.dar_likes()
@@ -41,5 +51,8 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    programa.imprime()
