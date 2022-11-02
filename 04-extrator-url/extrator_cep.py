8fe131eedd8f609cdcf191a654aeb8f56652532b
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2022-11-02 12:12:20 -0300

    Improving URL validation

--- 04-extrator-url/extrator_cep.py
+++ 04-extrator-url/extrator_cep.py
@@ -15,3 +15,34 @@ busca = padrao_cep.search(endereco)  # Retorna Match ou None
 if busca:
     cep = busca.group()  # Retorna a str encontrada no padrão criado
     print(cep)
+
+# ---------------------------------
+
+'''
+Verifica se a base da URL está de acordo com o padrão correto.
+
+Exemplos de URLs válidas:
+    bytebank.com/cambio
+    bytebank.com.br/cambio
+    www.bytebank.com/cambio
+    www.bytebank.com.br/cambio
+    http://www.bytebank.com/cambio
+    http://www.bytebank.com.br/cambio
+    https://www.bytebank.com/cambio
+    https://www.bytebank.com.br/cambio
+
+Exemplos de URLs inválidas:
+    https://bytebank/cambio
+    https://bytebank.naoexiste/cambio
+    ht://bytebank.naoexiste/cambio
+'''
+
+url = 'www.bytebank.com.br/cambio'
+
+padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+match = padrao_url.match(url)
+
+if not match:
+    raise ValueError('A URL não é válida.')
+
+print('A URL é válida.')
--- 04-extrator-url/extrator_url.py
+++ 04-extrator-url/extrator_url.py
@@ -1,3 +1,5 @@
+import re
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -13,6 +15,11 @@ class ExtratorURL:
     def valida_url(self):
         if not self.url:  # Se a url for vazia, retorna falso e é negada,  resultando em erro:
             raise ValueError('A URL está vazia')
+
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(self.url)
+        if not match:
+            raise ValueError('A URL não é válida.')
 
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
