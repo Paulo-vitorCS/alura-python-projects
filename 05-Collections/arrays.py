629189a265c0336902041f9c5d7f7a0d2ddd6609
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2022-11-10 10:30:31 -0300

    Learning about enumerate

--- 05-Collections/arrays.py
+++ 05-Collections/arrays.py
@@ -8,4 +8,36 @@ numeros = np.array([1, 3.5])
 print(numeros)
 
 numeros += 3
-print(numeros)
+print(numeros, '\n')
+
+idades = [13, 25, 32, 45, 54, 23, 32, 84]
+
+x = list(range(0, len(idades)))  # Forçando a geração de valores
+print(x)
+
+y = list(enumerate(idades))  # Utilizando enumerate para gerar uma lista com a posição e o valor
+print(y, '\n')
+
+for i in enumerate(idades):
+    print(i)
+
+print()
+
+for indice, idade in enumerate(idades):
+    print(f'{indice}: {idade}')
+
+print()
+
+usuarios = [
+    ('Guilherme', 37, 1981),
+    ('Daniel', 25, 1997),
+    ('Marcella', 23, 1999)
+]
+
+for nome, idade, nascimento in usuarios:  # Desempacontando
+    print(nome, end='\t')
+
+print()
+
+for nome, _, _ in usuarios:  # Desempacotando nome e ignorando o resto
+    print(nome, end='\t')
