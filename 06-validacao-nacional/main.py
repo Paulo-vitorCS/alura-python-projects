cc6c1292823e4bb10e2ea3c96a82f073bc4ec296
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2022-11-23 11:14:11 -0300

    Validating phone number with Regex

--- 06-validacao-nacional/main.py
+++ 06-validacao-nacional/main.py
@@ -1,4 +1,5 @@
 from cpf_cnpj import Documento
+from telefoneBr import TelefonesBr
 
 cpf = '32007832062'
 exemplo_cpf = Documento.criar_documento(cpf)
@@ -6,4 +7,9 @@ print(exemplo_cpf)
 
 cnpj = '35379838000112'
 exemplo_cnpj = Documento.criar_documento(cnpj)
-print(exemplo_cnpj)
+print(exemplo_cnpj, '\n')
+# ------------------------------------------------------
+
+telefone = '552126451234'
+telefone_objeto = TelefonesBr(telefone)
+print(telefone_objeto)
